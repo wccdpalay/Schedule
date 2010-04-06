828b6507cf5ffc0f060a47010da69a0b326eef97
unknown <dpalay@.labs.is.wccnet.org>
2010-04-06 14:52:48 -0400

    updated database info
    Signed-off-by: wccdpalay <dpalay@wccnet.org>

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,22 +2,28 @@ set :application, "Schedule"
 set :deploy_to, "/Users/adminosx/Sites/Schedule"
 set :deploy_via, :export
 
+set :rake, '/usr/local/bin/rake'
+set :ruby,'/usr/local/bin/ruby'
+set :gem, '/usr/local/bin/gem'
+
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :repository,  "git@github.com:wccdpalay/Schedule.git"
 set :branch, "master"
-
+set :scm_command, "/usr/local/git/bin/git"
+set :local_scm_command, "git"
 
 default_run_options[:pty] = true
 
 set :ssh_options, {:forward_agent => true}
-set :user, adminosx
+set :user, "adminosx"
 set :group, "admin"
 
-role :web, "serafina.is.wccnet.org"                          # Your HTTP server, Apache/etc
-role :app, "serafina.is.wccnet.org"                          # This may be the same as your `Web` server
-role :db,  "serafina.is.wccnet.org", :primary => true # This is where Rails migrations will run
+
+role :web, "207.72.2.8"                          # Your HTTP server, Apache/etc
+role :app, "207.72.2.8"                          # This may be the same as your `Web` server
+role :db,  "207.72.2.8", :primary => true # This is where Rails migrations will run
 
 
 # If you are using Passenger mod_rails uncomment this:
