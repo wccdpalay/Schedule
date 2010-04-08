6fde12c92538f07b8e652f01ecfae38df62809c9
unknown <dpalay@.labs.is.wccnet.org>
2010-04-08 16:36:34 -0400

    Signed-off-by: unknown <dpalay@.labs.is.wccnet.org>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "Schedule"
-set :deploy_to, "/Users/adminosx/Sites/Schedule"
+set :deploy_to, "/Users/adminosx/Sites/#{application}"
 set :deploy_via, :export
 
 set :rake, '/usr/local/bin/rake'
@@ -9,7 +9,7 @@ set :gem, '/usr/local/bin/gem'
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-set :repository,  "git@github.com:wccdpalay/Schedule.git"
+set :repository,  "git@github.com:wccdpalay/#{application}.git"
 set :branch, "master"
 set :scm_command, "/usr/local/git/bin/git"
 set :local_scm_command, "git"
@@ -31,32 +31,18 @@ role :db,  "207.72.2.8", :primary => true # This is where Rails migrations will 
 # these http://github.com/rails/irs_process_scripts
 
 
-set :mongrel_cmd, "/usr/bin/mongrel_rails_persist"
-set :mongrel_ports, 3000..3001
-
-
 
 namespace :deploy do
-
-  desc "Start Mongrels processes and add them to launchd."
   task :start, :roles => :app do
-    mongrel_ports.each do |port|
-      sudo "#{mongrel_cmd} start -p #{port} -e production \
-            --user #{user} --group #{group} -c #{current_path}"
-    end
+    run "touch #{current_release}/tmp/restart.txt"
   end
 
-  desc "Stop Mongrels processes and remove them from launchd."
   task :stop, :roles => :app do
-    mongrel_ports.each do |port|
-      sudo "#{mongrel_cmd} stop -p #{port}"
-    end
+    # Do nothing.
   end
 
-  desc "Restart Mongrel processes"
+  desc "Restart Application"
   task :restart, :roles => :app do
-    stop
-    start
+    run "touch #{current_release}/tmp/restart.txt"
   end
- 
 end
