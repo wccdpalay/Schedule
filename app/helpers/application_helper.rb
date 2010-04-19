d0d9d6265e8b83810df003e1f2e1145fc6c0bd79
unknown <dpalay@.labs.is.wccnet.org>
2010-04-19 12:45:55 -0400

    Signed-off-by: unknown <dpalay@.labs.is.wccnet.org>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def admin?
-    session[:user].usertype == "Administrator"
+    session[:user].usertype != "Administrator"
   end
   
   def link_to_cal(string, date, length="day")
