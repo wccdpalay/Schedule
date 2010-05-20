fe4433e9927e1980e59840dd799996462dddabb8
dpalay <dpalay@wccnet.org>
2010-05-20 13:15:37 -0400

    bug fixes

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -15,28 +15,30 @@ class TemplateController < ApplicationController
     @dtemplate = Dtemplate.find(params[:id] || :last)
   end
 
-  def update_day
-    @day = Dtemplate.find_by_id(params[:id])
-    @slots = [params[:slotAs],params[:slotBs],params[:slotCs],params[:slotDs]]
-    @dslots = [@day.slotAs, @day.slotBs, @day.slotCs, @day.slotDs]
-    for col in 0..3
-      for slot in @slots[col]
-        dslot = @dslots[col][slot[0].to_i]
-        if slot[1] != 'nil'
-          dslot.user_id = slot[1]
-        else
-          dslot.user_id = nil
-        end
-        dslot.save!        
-      end
-    end
-    @day.save!
-    respond_to do |format|
-      format.html {redirect_to :action => "day", :id => params[:id]}
-      format.js  {}
-    end
+  def list_weeks
+
+  end
+
+  def list_days
+
   end
   
+  def edit_week
+
+  end
+
+  def edit_day
+
+  end
+
+  def delete_week
+
+  end
+
+  def delete_day
+    
+  end
+
   def new_week
     @week = Wtemplate.new()
     @week.name = params[:wtemplate][:name]
@@ -65,16 +67,8 @@ class TemplateController < ApplicationController
       format.js {}
     end
   end
-  
-  def change_day
-    @day = Dtemplate.find(params[:value])
-    params[:id] = params[:value]
-    respond_to do |format|
-      format.html {redirect_to :action => "day", :id => @day}
-      format.js  {}
-    end
-  end
-  
+
+
   def change_week
     @week = Wtemplate.find(params[:value])
     params[:id] = params[:value]
@@ -84,9 +78,19 @@ class TemplateController < ApplicationController
     end
   end
 
+  def change_day
+    @day = Dtemplate.find(params[:value])
+    params[:id] = params[:value]
+    respond_to do |format|
+      format.html {redirect_to :action => "day", :id => @day}
+      format.js  {}
+    end
+  end
+  
+
+
   def update_week
     @wtemplate = Wtemplate.find(params[:id])
-
     @wtemplate[:sat] = params[:day]["1"]
     @wtemplate[:sun] = params[:day]["2"]
     @wtemplate[:mon] = params[:day]["3"]
@@ -94,14 +98,32 @@ class TemplateController < ApplicationController
     @wtemplate[:wed] = params[:day]["5"]
     @wtemplate[:thu] = params[:day]["6"]
     @wtemplate[:fri] = params[:day]["7"]
-
     @wtemplate.save!
-
-
-
     respond_to do |format|
       format.html {redirect_to :action => "week", :id => @wtemplate.id}
+      format.js  {}
+    end
+  end
 
+
+  def update_day
+    @day = Dtemplate.find_by_id(params[:id])
+    @slots = [params[:slotAs],params[:slotBs],params[:slotCs],params[:slotDs]]
+    @dslots = [@day.slotAs, @day.slotBs, @day.slotCs, @day.slotDs]
+    for col in 0..3
+      for slot in @slots[col]
+        dslot = @dslots[col][slot[0].to_i]
+        if slot[1] != 'nil'
+          dslot.user_id = slot[1]
+        else
+          dslot.user_id = nil
+        end
+        dslot.save!
+      end
+    end
+    @day.save!
+    respond_to do |format|
+      format.html {redirect_to :action => "day", :id => params[:id]}
       format.js  {}
     end
   end
